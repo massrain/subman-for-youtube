f1abd5a9ce8ce249210bf48ae34af21fd25c990b
Mevlut Canvar <mevlutcanvar@gmail.com>
2018-10-11 00:09:39 +0300

    creates nonfunctional, basic options page

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,96 +1,241 @@
 <template>
-    <div>
-        <nav class="level">
-            <div class="level-item has-text-centered">
-                <div>
-                    {{ state }}
-                    <input class="input"
-                           v-model="category"
-                           @keyup.enter.prevent="addCategory"
-                           type="text"
-                           placeholder="Add New Category">
+    <div :class="{'modal-open': modalVisible}" v-if="authorized">
+        <navbar></navbar>
+
+        <section style="padding-bottom:100px">
+            <div class="container-fluid" style="margin-top:10px;padding-bottom:100px">
+
+                <div class="row justify-content-center">
+                    <h2>Extension specific options</h2>
+                </div>
+
+                <hr noshade class="mt-2">
+
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-8">
+                            <form>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <h6>Sync my custom categories to Chrome bookmarks.</h6>
+                                    </div>
+                                    <div class="col-4">
+                                        <input class="form-control" type="checkbox" disabled>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <h6>SubMan Bookmarks folder name:</h6>
+                                    </div>
+                                    <div class="col-4">
+                                        <input class="form-control"
+                                               value="SubMan"
+                                               type="text" disabled>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </section>
 
-        <category v-for="(category, index) in categories"
-                  @category-removed="deleteCategory(index)"
-                  @subscriptions-updated="syncCategories()"
-                  :key="category.slug"
-                  :data="category"></category>
+        <modal type="category"></modal>
+
+        <section>
+            <div class="footer sticky-bottom text-danger">
+                <strong>SubMan for YouTube - 2018</strong>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
-    import Category from './Category';
-    import slugify from 'slugify'
+    import state from '../mixins/state'
+    import API_KEY from '../credentials.json'
+
+    import Navbar from './Navbar'
 
     export default {
         name: "App",
+        mixins: [state],
         components: {
-            Category
+            Navbar
         },
         data: function () {
             return {
-                items: [],
-                category: '',
-                categories: []
-            }
-        },
-        watch: {
-            categories(newCategories) {
-                chrome.storage.sync.set({categories: newCategories});
-            }
-        },
-        computed: {
-            categorySlug() {
-                return slugify(this.category);
-            },
-            state() {
-                chrome.storage.sync.get('STATE_AUTHORIZED', function(result) {
-
-                    if (result.STATE_AUTHORIZED) {
-                        return 'yeap';
-                    }
-
-                    return 'nope';
-                });
+                bookmarksRootId: null
             }
         },
         methods: {
-            addCategory() {
-                this.categories.push({
-                    slug: this.categorySlug,
-                    name: this.category,
-                    subscriptions: []
+            syncStorage(newCategories = this.categories) {
+                chrome.storage.sync.set({categories: newCategories});
+
+                if (this.bookmarksRootId) this.syncBookmarksFolder();
+            },
+            createBookmarksFolderIfNotExists() {
+                let self = this;
+                chrome.bookmarks.search({ title: 'SubMan' },function (results) {
+
+                    if (results.length === 0) {
+                        chrome.bookmarks.create({
+                            index: 0,
+                            parentId: "1",
+                            title: 'SubMan'
+                        }, result => {
+                            self.bookmarksRootId = result.id;
+                        });
+
+                        return;
+                    }
+
+                    self.bookmarksRootId = results[0].id;
+                });
+            },
+            syncBookmarksFolder() {
+                let self = this;
+
+                chrome.bookmarks.getChildren(this.bookmarksRootId, childs => {
+                    childs.forEach(channel => chrome.bookmarks.removeTree(channel.id) );
                 });
 
-                this.category = '';
-            },
-
-            deleteCategory(id) {
-                this.categories.splice(id, 1);
-            },
-            syncCategories() {
-                chrome.storage.sync.set({categories: this.categories}, function() {});
+                this.categories.forEach((category, index) => {
+                    chrome.bookmarks.create({
+                        index: index,
+                        parentId: self.bookmarksRootId,
+                        title: category.name
+                    }, result => {
+                        self.categories[result.index].channels.forEach(channel => {
+                            chrome.bookmarks.create({
+                                parentId: result.id,
+                                title: channel.snippet.title,
+                                url: 'https://youtube.com/channel/' + channel.snippet.resourceId.channelId
+                            }, result => {});
+                        })
+                    });
+                });
             }
         },
+        created() {
+            // let self = this;
+            // chrome.storage.sync.get('categories', function (result) {
+            //
+            //     if (result.categories) {
+            //         self.setCategories(result.categories);
+            //
+            //         return;
+            //     }
+            //
+            //     self.setCategories([]);
+            // });
+        },
         mounted() {
-            let self = this;
-            chrome.storage.sync.get('categories', function(result) {
+            // this.requestAllSubscriptions();
 
-                if (result.categories) {
-                    self.categories = result.categories;
-
-                    return;
-                }
-
-                self.categories = [];
-            });
+            // this.createBookmarksFolderIfNotExists ();
         }
     };
 </script>
 
-<style lang="sass">
-    /*@import "~bulma"*/
+<style>
+    @import "~bootstrap/dist/css/bootstrap.min.css";
+
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: white;
+        color: white;
+        text-align: center;
+    }
+
+    .animate-spin {
+        -moz-animation: spin 2s infinite linear;
+        -o-animation: spin 2s infinite linear;
+        -webkit-animation: spin 2s infinite linear;
+        animation: spin 2s infinite linear;
+        display: inline-block;
+    }
+
+    @-moz-keyframes spin {
+        0% {
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -moz-transform: rotate(359deg);
+            -o-transform: rotate(359deg);
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    @-webkit-keyframes spin {
+        0% {
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -moz-transform: rotate(359deg);
+            -o-transform: rotate(359deg);
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    @-o-keyframes spin {
+        0% {
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -moz-transform: rotate(359deg);
+            -o-transform: rotate(359deg);
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    @-ms-keyframes spin {
+        0% {
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -moz-transform: rotate(359deg);
+            -o-transform: rotate(359deg);
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    @keyframes spin {
+        0% {
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -moz-transform: rotate(359deg);
+            -o-transform: rotate(359deg);
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
 </style>
---
+++ src/options/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <section name="NAVBAR">
+        <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #cc181e; padding-bottom: 0;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="#">SubMan for YouTube</a>
+                <ul class="navbar-nav nav nav-tabs ml-auto mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active bg-outline-light text-danger" href="#">
+                            <strong>General Options</strong>
+                        </a>
+                    </li>
+                </ul>
+                <a class="nav-link text-white" href="https://massrain.github.io/subman-for-youtube/">About/Contact</a>
+
+            </div>
+        </nav>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        props: [],
+        data: function () {
+            return {}
+        },
+        methods: {},
+        mounted() {
+        }
+    }
+</script>
+
+<style>
+    .navbar {
+        padding-bottom: 0 !important;
+    }
+</style>
